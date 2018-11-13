5ab35350328b20df7733fff2646be8c336fa2aaf
Xander Ge <xander.q.ge@gmail.com>
2018-11-12 19:21:14 -0600

    Converted to Use TeleDrive (Two Stick)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisIntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisIntakeTest.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 @TeleOp(name="ChassisIntakeTest", group="Teleop")
 public class ChassisIntakeTest extends OpMode{
     private Drive drive;
+    private TeleDrive teleDrive;
     private DcMotor intakeR;
     private DcMotor intakeL;
 
@@ -21,23 +22,10 @@ public class ChassisIntakeTest extends OpMode{
         intakeL = hardwareMap.dcMotor.get("intakeL");
 
         drive = new Drive(fl,bl,br,fr);
+        teleDrive = new TeleDrive(drive, gamepad1);
     }
     public void loop() {
-        double throttle = gamepad1.left_stick_y;
-        double direction = gamepad1.left_stick_x;
-        double leftPower = 0;
-        double rightPower = 0;
-
-        if(Math.abs(throttle) > 0.1 || Math.abs(direction) > 0.1) {
-            leftPower = throttle+direction;
-            rightPower = throttle-direction;
-
-            drive.SetLeftMotors(leftPower);
-            drive.SetRightMotors(rightPower);
-        } else {
-            drive.SetLeftMotors(0.0);
-            drive.SetRightMotors(0.0);
-        }
+        teleDrive.Update();
 
         boolean intake = gamepad1.a;
 
