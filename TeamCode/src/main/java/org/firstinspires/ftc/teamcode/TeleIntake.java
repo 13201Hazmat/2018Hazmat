9a6032e3094d3c5878d3c4165a89569a2b01207b
Xander Ge <xander.q.ge@gmail.com>
2018-11-26 19:01:25 -0600

    Added Intake to TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleIntake.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 public class TeleIntake {
+    private Intake intake;
+    private Gamepad controller;
+    public TeleIntake(Intake in, Gamepad c) {
+        intake = in;
+        controller = c;
+    }
+
+    public void update() {
+        boolean suck = controller.right_trigger > 0.5;
+        if (suck) {
+            intake.setIntakeSpeed(1);
+        }
+
+    }
 }
