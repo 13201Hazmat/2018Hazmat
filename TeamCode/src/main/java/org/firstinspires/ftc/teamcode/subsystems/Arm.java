109e359ac8fc1a65f581a27684c72a536e5cddf9
Tyler Tran <19trant@elmbrookstudents.org>
2018-12-03 18:38:49 -0600

    Wrote arm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,5 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Arm {
+    private final int BOTTOM = 0;
+    private final int MIDDLE = 0;
+    private final int TOP = 0;
+    private DcMotor armMotor;
+    private boolean upDog;
+
+    public Arm(DcMotor motor) {
+        armMotor = motor;
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(.5);
+        armMotor.setTargetPosition(BOTTOM);
+    }
+
+    public void setArm(boolean direction) {
+        upDog = direction;
+    }
+
+    public void update() {
+        if (upDog) {
+            armMotor.setTargetPosition(TOP);
+        } else {
+            armMotor.setTargetPosition(BOTTOM);
+        }
+    }
 
 }
