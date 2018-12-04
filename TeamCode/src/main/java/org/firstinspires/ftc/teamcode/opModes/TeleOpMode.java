7a3711942a2ef2f1537ea5f300917d7a6df3d324
Tyler Tran <19trant@elmbrookstudents.org>
2018-12-03 19:21:12 -0600

    Added everything into TeleOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOpMode.java
@@ -5,12 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.teleOp.TeleArm;
 import org.firstinspires.ftc.teamcode.teleOp.TeleDrive;
 import org.firstinspires.ftc.teamcode.teleOp.TeleIntake;
 
-@TeleOp(name="ChassisTest", group="Teleop")
+@TeleOp(name="TeleOpMode", group="Teleop")
 public class TeleOpMode extends LinearOpMode {
     public void runOpMode () {
         DcMotor FrontLeftMotor = hardwareMap.dcMotor.get("front_left_motor");
@@ -18,17 +20,23 @@ public class TeleOpMode extends LinearOpMode {
         DcMotor FrontRightMotor = hardwareMap.dcMotor.get("front_right_motor");
         DcMotor BackRightMotor = hardwareMap.dcMotor.get("back_right_motor");
         DcMotor intakeMotor = hardwareMap.dcMotor.get("intake_motor");
-        Servo phantomServo = null;
+        DcMotor armMotor = hardwareMap.dcMotor.get("arm_motor");
+        Servo intakeServo = hardwareMap.servo.get("intake_servo");
 
         Drive drive = new Drive(FrontLeftMotor, BackLeftMotor, BackRightMotor, FrontRightMotor);
         TeleDrive teleDrive = new TeleDrive(drive, gamepad1);
 
-        Intake intake = new Intake(intakeMotor, phantomServo);
+        Intake intake = new Intake(intakeMotor, intakeServo);
         TeleIntake teleIntake = new TeleIntake(intake, gamepad1);
+
+        Arm arm = new Arm(armMotor);
+        TeleArm teleArm = new TeleArm(arm, gamepad1);
+
         waitForStart();
         while(opModeIsActive()){
             teleDrive.Update();
             teleIntake.update();
+            teleArm.update();
         }
     }
 }
