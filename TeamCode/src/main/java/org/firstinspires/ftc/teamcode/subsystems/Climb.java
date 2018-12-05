c96f62caa1976a8b28f0ba9970b82f759f8054a4
Tyler Tran <19trant@elmbrookstudents.org>
2018-12-04 19:13:33 -0600

    Created Climb Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climb.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Climb {
+    private DcMotor motor;
+
+    public Climb(DcMotor motor){
+        this.motor = motor;
+    }
+}
