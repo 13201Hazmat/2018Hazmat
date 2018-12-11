7defca2f4d8d9e0159ba0c635a8a9cb8369fbcde
Tyler Tran <19trant@elmbrookstudents.org>
2018-12-10 19:01:23 -0600

    Climb method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climb.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climb.java
@@ -4,16 +4,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Climb {
     private DcMotor motor;
+    private boolean direction = false;
 
     public Climb(DcMotor motor) {
         this.motor = motor;
     }
-
-    public void down() {
-
-    }
-
-    public void up() {
-    
+    public void setClimb() {
+        if (direction == false) {
+            motor.setPower(-0.5);
+        }
+        if (direction == true) {
+            motor.setPower(0.5);
+        }
     }
 }
