4e8e3d13c25b171875e2b8135d99afc8d5ad9d6f
Tyler Tran <19trant@elmbrookstudents.org>
2018-12-10 19:12:14 -0600

    ClimbCommand

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/commands/ClimbCommand.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.auto.commands;
+
+import org.firstinspires.ftc.teamcode.auto.ICommand;
+
+public class ClimbCommand implements ICommand {
+    private Climb climb;
+
+    @Override
+    public boolean runCommand() {
+        return false;
+    }
+}
