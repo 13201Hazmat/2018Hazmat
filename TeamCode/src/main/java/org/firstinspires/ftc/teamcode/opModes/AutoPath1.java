81dde96dbc18297072365a043ba0abd3ef8f1517
Tyler Tran <19trant@elmbrookstudents.org>
2019-02-02 12:59:29 -0600

    AutoPath1, AutoPath3, AutoPath4, AutoPath7 all working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoPath1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoPath1.java
@@ -76,16 +76,16 @@ public class AutoPath1 extends OpMode {
 
         commands = new ArrayList<ICommand>();
         commands.add(new ClimbCommand(climber, true));
-        //commands.add(new DriveCommand(drive, 4800, 1));
         commands.add(new IntakeCommand(intake, -1, .6,.4));
         commands.add(new DriveCommand(drive, 2400, 1));
         commands.add(new DriveCommand(drive, 2400, 1));
         commands.add(new IntakeCommand(intake, 0,-1,1));
-        commands.add(new DriveCommand(drive, 1250, -1));
-        commands.add(new TurnCommand(drive, 1, 45, imu));
-        commands.add(new DriveCommand(drive, 1250, 1));
-        commands.add(new TurnCommand(drive, 1,135,imu));
-        commands.add(new DriveCommand(drive,4300,1));
+        commands.add(new DriveCommand(drive, 3250, -1));
+        commands.add(new TurnCommand(drive, 1, 95, imu));
+        //commands.add(new DriveCommand(drive,4300,1));
+        commands.add(new DriveCommand(drive, 3250, 1));
+        //commands.add(new TurnCommand(drive, 1 , , imu));
+        //commands.add(new DriveCommand(drive, 2150, 1));
         commands.add(new IntakeCommand(intake, .2,.6,.4));
 
         climbingCommand = new ClimbCommand(climber, false);
